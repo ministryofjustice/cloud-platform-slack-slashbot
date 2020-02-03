180a27702b6862bf2f6b7747470708c356cd170b
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2020-02-03 13:05:18 +0000

    Update for more slash commands

--- app.rb
+++ app.rb
@@ -7,13 +7,40 @@ require_relative 'slack_messenger'
 
 use SlackAuthorizer
 
-HELP_RESPONSE = "Cloud Platform has below slash commands\n" \
+CPHELP_RESPONSE = "Cloud Platform has below slash commands\n" \
                 "/cpopenhours [day] [time]\n" \
+                "/cpauthenticate\n"
+                "/cpalerts\n"
+                "/cprdsmigrate\n"
+                "/cpgitcrypt\n"
+                "/cptrouble\n"
+                "/cpdsd\n"
                 "/cp\n"
 
-OPEN_HOURS_EXPRESSION = /^([\w\.\-_]+) ([\w\.\-_]+) (.+)/
+CPAUTHENTICATE_RESPONSE = "Here is the process for authenticating into the live cluster.\n" \
+                        "https://user-guide.cloud-platform.service.justice.gov.uk/documentation/getting-started/kubectl-config.html#authentication.\n" \
+                        "Does this help?"
 
-OPEN_HOURS_RESPONSE = "Cloud Platform Open Hour (and a half!) - Got questions about the Cloud Platform? Want to know more about key concepts? Questions about deployment strategy? How to improve your build time? Have a need not already being met by the Platform? Well, come and talk to us!
+CPALERTS_RESPONSE = "Here is the guide for creating custom alerts for your applications. \n" \
+                    "https://user-guide.cloud-platform.service.justice.gov.uk/documentation/monitoring-an-app/how-to-create-alarms.html#creating-your-own-custom-alerts.\n" \
+                    "Does this help?"
+
+CPRDSMIGRATE_RESPONSE = "Here is the guide for migrating your RDS instance.  \n" \
+                        "https://user-guide.cloud-platform.service.justice.gov.uk/documentation/other-topics/aws-rds-migration.html#migrating-an-rds-instance.\n" \
+                        "Does this help?"
+
+CPGITCRYPT_RESPONSE = "Here is the guide to encrypt application secrets using git-crypt. \n" \
+                      "https://user-guide.cloud-platform.service.justice.gov.uk/documentation/other-topics/git-crypt-setup.html#git-crypt. \n" \
+                      "Does this help?"
+
+CPTROUBLE_RESPONSE = "I think the information you need might be in the troubleshooting guide here:  \n" \
+                     "https://user-guide.cloud-platform.service.justice.gov.uk/documentation/other-topics/troubleshooting.html#troubleshooting-guide \n"
+
+CPDSD_RESPONSE = "That's handled by the digital service desk, not the cloud platform team. Please ask in #digitalservicedesk \n"
+
+CPOPENHOURS_EXPRESSION = /^([\w\.\-_]+) ([\w\.\-_]+) (.+)/
+
+CPOPENHOURS_RESPONSE = "Cloud Platform Open Hour (and a half!) - Got questions about the Cloud Platform? Want to know more about key concepts? Questions about deployment strategy? How to improve your build time? Have a need not already being met by the Platform? Well, come and talk to us!
 Our Open “Hour” is [time] this Friday [day].
 If you would like a 30 minute slot please reply to this tread. We’ll confirm your needs prior to the session.
 We hope to make these a regular feature so don’t despair if you miss out this time.
@@ -27,10 +54,22 @@ post '/slack/command' do
   case params['command']
   when '/cp' then
     message = HELP_RESPONSE
+  when '/cpauthenticate' then
+    message = CPAUTHENTICATE_RESPONSE
+  when '/cpalerts' then
+    message = CPALERTS_RESPONSE
+  when '/cprdsmigrate' then
+    message = CPRDSMIGRATE_RESPONSE
+  when '/cpgitcrypt' then
+    message = CPGITCRYPT_RESPONSE
+  when '/cptrouble' then
+    message = CPTROUBLE_RESPONSE
+  when '/cpdsd' then
+    message = CPDSD_RESPONSE
   when '/cpopenhours' then
-    open_hours_response = OPEN_HOURS_RESPONSE
+    open_hours_response = CPOPENHOURS_RESPONSE
     case params['text']
-    when OPEN_HOURS_EXPRESSION
+    when CPOPENHOURS_EXPRESSION
       day, time = $1 + ' ' + $2, $3
       open_hours_response.gsub!("[day]",day)
       open_hours_response.gsub!("[time]",time)
