fd816ff2ecfe245ec5c7ab1e90f84787383d29f8
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2020-02-06 11:54:33 +0000

    update token to env variable and exposed secret rotated

--- slack_authorizer.rb
+++ slack_authorizer.rb
@@ -18,7 +18,7 @@ class SlackAuthorizer
   
     def call(env)
       req = Rack::Request.new(env)
-      if req.params['token'] == "Rkhx32kIMr8uAfx6f1lwiTss"
+      if req.params['token'] == ENV['SLACK_TOKEN']
         @app.call(env)
       else
         UNAUTHORIZED_RESPONSE
